c6c01a27cb4d6d2fa2ecae7a3be7d36455c95800
mariob0y <69054485+mariob0y@users.noreply.github.com>
2020-10-22 14:30:22 +0300

    Update scrap.py

--- post/scrap.py
+++ post/scrap.py
@@ -9,10 +9,12 @@ from django.db import IntegrityError
 
 url = 'https://www.reddit.com/r/memes/new/'
 
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument(" - incognito")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
