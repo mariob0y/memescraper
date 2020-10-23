dae4225d0a661bafc13c4c3ae5053ce9dc020ae3
mariob0y <69054485+mariob0y@users.noreply.github.com>
2020-10-23 12:51:32 +0300

    Update views.py

--- post/views.py
+++ post/views.py
@@ -3,24 +3,6 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 from .models import Post
-
-
-def home(request):
-	posts = Post.objects.order_by('date').reverse()
-	paginator = Paginator(posts, 18)
-	page_number = request.GET.get('page')
-	posts = paginator.get_page(page_number)
-
-	return render(request, "home.html", {'posts': posts})
-
-def random(request):
-	posts = Post.objects.order_by('?')
-	paginator = Paginator(posts, 18)
-	page_number = request.GET.get('page')
-	posts = paginator.get_page(page_number)
-
-	return render(request, "home.html", {'posts': posts})
-
 from django.views.generic.list import ListView
 
 class PostsView(ListView):
